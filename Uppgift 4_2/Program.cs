1b50aeff9c72f768ce8adaa12cc38e0e81ebfeb4
ival06001 <ival06001@E-PW024NVZ>
2023-10-06 10:52:47 +0200

    Add project files.

---
+++ Uppgift 4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Uppgift_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Om din låt är exempelvis 3 minuter och 20 sekunder lång, så ska du skriva den som 320. Varsågod.");
+            string låt = Console.ReadLine();
+            int längd = int.Parse(låt);
+
+            switch (längd)
+            {
+                case 1:
+                    if (längd >= 245 && längd <= 420)
+                    {
+                        Console.WriteLine("Okej, din låt passar bra.");
+                    }
+                    break;
+                        case 2:
+                    if (längd < 245 && längd > 420)
+                    {
+                        Console.WriteLine("Din låt passar ej bra");
+                    }
+                    break;
+            }
+            
+            
+
+        }
+    }
+}
